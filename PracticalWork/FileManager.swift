7dd62aac2bb468f8a863e91b87cca80057016f2d
Ivan Kopiev <kopiev@mustdev.ru>
2021-06-16 19:32:43 +0300

    Start PR

---
+++ PracticalWork/FileManager.swift
@@ -0,0 +1,28 @@
+//
+//  FileManager.swift
+//  PracticalWork
+//
+//  Created by Ivan Kopiev on 16.06.2021.
+//
+
+import Foundation
+
+func saveToFile() {
+    guard let pathOut = Bundle.main.path(forResource: "out", ofType: "txt") else {return}
+    do {
+        try "vanes".write(toFile: pathOut, atomically: true, encoding: .utf8)
+    } catch let error {
+        print(error.localizedDescription)
+    }
+}
+
+func readFromFile() -> String {
+    guard let pathOut = Bundle.main.path(forResource: "out", ofType: "txt") else {return "failed to get path"}
+    do {
+        let string = try String(contentsOfFile: pathOut, encoding: String.Encoding.utf8)
+        return string
+    } catch let error {
+        print(error.localizedDescription)
+        return "failed to get file"
+    }
+}
--- PracticalWork/main.swift
+++ PracticalWork/main.swift
@@ -2,10 +2,11 @@
 //  main.swift
 //  PracticalWork
 //
-//  Created by Ivan Kopiev on 16.06.2021.
+//  Created by Yaroslav Bogomolov on 16.05.2021.
 //
 
 import Foundation
 
-print("Hello, World!")
 
+saveToFile()
+print(readFromFile())
